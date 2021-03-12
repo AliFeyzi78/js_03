3331116f409c8397443a42a9fc48bf6eeca72fd4
AliFeyzi78 <zeron1g2k@gmail.com>
2021-03-13 01:25:13 +0200

    finish session-2

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello,    World');
+/* eslint-disable linebreak-style */
+// /* eslint-disable linebreak-style */
+// document.write('Hello\n<br>');
+// document.write('clarusway');
+// console.log('Hello\n');
+// console.log('cw');
+// alert('hello\n clarusway');
+// confirm('hello');
+// prompt('yaşınızı girin', 17);
+
+document.body.innerHTML = '<h1> Clarusway Rocks </h1>';
+// document.head.innerHTML = '<title>cw</title>';
+
+console.trace();
+console.time();
+console.log('log');
+console.error('error');
+console.warn('warn');
+console.info('info');
+console.timeEnd();
