bc13f33fe39e6f64133c84aa08f57417b50ee0d7
AliFeyzi78 <zeron1g2k@gmail.com>
2021-03-20 20:14:54 +0200

    finish if

--- app.js
+++ app.js
@@ -8,13 +8,41 @@
 // confirm('hello');
 // prompt('yaşınızı girin', 17);
 
-document.body.innerHTML = '<h1> Clarusway Rocks </h1>';
-// document.head.innerHTML = '<title>cw</title>';
+// document.body.innerHTML = '<h1> Clarusway Rocks </h1>';
+// // document.head.innerHTML = '<title>cw</title>';
 
-console.trace();
-console.time();
-console.log('log');
-console.error('error');
-console.warn('warn');
-console.info('info');
-console.timeEnd();
+// console.trace();
+// console.time();
+// console.log('log');
+// console.error('error');
+// console.warn('warn');
+// console.info('info');
+// console.timeEnd();
+
+// var contry = prompt('enter your contry: ');
+// alert('you are from ' + country);
+
+// console.log('outside');
+
+// var a = 3;
+
+// console.log({ a });
+
+// a = 4;
+// var b;
+// console.log(b);
+// b = 4;
+// console.log([b]);
+
+// console.log(a);
+
+// var a = 3;
+
+// ***********IF Statement*******
+let num1 = prompt('Enter a number: ');
+if (num1 > 0) {
+  alert('num1 is Positif');
+}
+let pathinfo = prompt('Please enter a path: ');
+
+pathinfo == "fs" ? 
